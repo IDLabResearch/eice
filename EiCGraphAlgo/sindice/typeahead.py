08ca90feb4d7b37bbeb37eb5361cff6ed7ee3822
ldevocht <laurensdv@gmail.com>
2012-11-05 11:45:50 +0100

    check if prefix in result label

--- EiCGraphAlgo/sindice/typeahead.py
+++ EiCGraphAlgo/sindice/typeahead.py
@@ -23,7 +23,7 @@ def dbPediaPrefix(prefix):
     root = lxml.objectify.fromstring(raw_output)
     results = list()
     for result in root.Result:
-        if hasattr(result.Classes, 'Class'):
+        if prefix in result.Label[0].text and hasattr(result.Classes, 'Class'):
             klasses = result.Classes.Class
             if hasattr(klasses, 'Label'):
                 klasse = klasses
