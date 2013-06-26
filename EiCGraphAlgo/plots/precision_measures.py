5dcd794b9c9cc519b8f5d741a0b679e7a8d6f27d
Laurens De Vocht <laurensdv@gmail.com>
2013-06-26 18:42:57 +0200

    measures fix

--- EiCGraphAlgo/plots/precision_measures.py
+++ EiCGraphAlgo/plots/precision_measures.py
@@ -17,26 +17,39 @@ queries = [
 searcher = Searcher()
 worker = Worker()
 resourceretriever = Resourceretriever()
-iterations = 4
+iterations = 5
 
 def handleResult(uri, target_uri, rankings):
     print('Looking for path between %s and %s' %(uri, target_uri))
     path = searcher.search(uri, target_uri, k=8)
     if path['path']:
         rank = len(path['path'])
-       
+        uris = rank/2
+        even = uris % 2 == 0
+        halved = False
+        half = round(uris/2)
+        
         s = 0
+        
         for step in path['path']:
             if s not in rankings:
                 rankings[s] = set()
             if 'node' in step['type']:
+                print (step['uri'])
+                print (s)
+                #print (halved)
+                #print (half)
                 rankings[s].add(step['uri'])
-                if s < rank/2:
+                if s < half and not halved:
                     s+=1
-                elif s == rank/2:
-                    s = rank/2
+                elif s == half:
+                    halved = True
+                    s-=1
+                elif s >= half or halved:
+                    s-=1
                 else:
                     s-=1
+                    
         print (rankings)
     
 worker.createQueue(handleResult)
@@ -85,27 +98,28 @@ for query in queries:
             expanded.add(uri)
             print (uri)
             neighbours = resourceretriever.getResource(uri)
-            for nb in neighbours:
-                neighbour = neighbours[nb]
-                subject = neighbour[0]
-                object = neighbour[2]
-                if subject == uri:
-                    new_uri = object
-                else:
-                    new_uri = subject
-                if not (minRank+1) in rankings:
-                    rankings[minRank+1] = set()
-                if '<' in new_uri:
-                    exists = False
-                    for rank in rankings:
-                        if new_uri.strip('<>') in rankings[rank]:
-                            exists = rank
-                    if not exists:
-                        rankings[minRank+1].add(new_uri.strip('<>'))
+            if neighbours:
+                for nb in neighbours:
+                    neighbour = neighbours[nb]
+                    subject = neighbour[0]
+                    obj = neighbour[2]
+                    if subject.strip('<>') == uri.strip('<>'):
+                        new_uri = obj
                     else:
-                        if exists > (minRank + 1):
+                        new_uri = subject
+                    if not (minRank+1) in rankings:
+                        rankings[minRank+1] = set()
+                    if '<' in new_uri and not 'XMLSchema' in new_uri:
+                        exists = False
+                        for rank in rankings:
+                            if new_uri.strip('<>') in rankings[rank]:
+                                exists = rank
+                        if not exists:
                             rankings[minRank+1].add(new_uri.strip('<>'))
-                            rankings[exists].remove(new_uri.strip('<>'))
+                        else:
+                            if exists > (minRank + 1):
+                                rankings[minRank+1].add(new_uri.strip('<>'))
+                                rankings[exists].remove(new_uri.strip('<>'))
                     
         print (rankings)
         i += 1
