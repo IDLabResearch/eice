eda8b74738261f902d7b6475ab7eb56c46e053f5
Laurens De Vocht <laurensdv@gmail.com>
2013-06-27 10:05:39 +0200

    measures

--- EiCGraphAlgo/plots/precision_measures.py
+++ EiCGraphAlgo/plots/precision_measures.py
@@ -19,6 +19,8 @@ worker = Worker()
 resourceretriever = Resourceretriever()
 iterations = 5
 
+avps=[]
+
 def handleResult(uri, target_uri, rankings):
     print('Looking for path between %s and %s' %(uri, target_uri))
     path = searcher.search(uri, target_uri, k=8)
@@ -40,11 +42,17 @@ def handleResult(uri, target_uri, rankings):
                 #print (halved)
                 #print (half)
                 rankings[s].add(step['uri'])
-                if s < half and not halved:
+                if s < half and not halved and not even:
                     s+=1
+                elif s < half-1 and not halved and even:
+                    s+=1
+                elif s == half-1 and even:
+                    halved = True
                 elif s == half:
                     halved = True
                     s-=1
+                elif even and halved:
+                    even = False
                 elif s >= half or halved:
                     s-=1
                 else:
@@ -103,13 +111,14 @@ for query in queries:
                     neighbour = neighbours[nb]
                     subject = neighbour[0]
                     obj = neighbour[2]
+                    pred = neighbour[1]
                     if subject.strip('<>') == uri.strip('<>'):
                         new_uri = obj
                     else:
                         new_uri = subject
                     if not (minRank+1) in rankings:
                         rankings[minRank+1] = set()
-                    if '<' in new_uri and not 'XMLSchema' in new_uri:
+                    if '<' in new_uri and not 'XMLSchema' in new_uri and not 'category' in new_uri:
                         exists = False
                         for rank in rankings:
                             if new_uri.strip('<>') in rankings[rank]:
@@ -126,4 +135,53 @@ for query in queries:
     print ('<--- Result for query:' % query)
     print (ujson.dumps(rankings))
     print ('--->')
+    
+    print ("Provide Relevancy scores")
+    
+    
+    relevancies = {}
+    
+    # Dummy generation
+    for rank in rankings:
+        rankings[rank] = list(rankings[rank])
+        relevancies[rank] = list()
+        relevancies[rank] = [random.choice([i for i in range(2)]) for r in range(len(rankings[rank]))]
+    print (ujson.dumps(relevancies))
+    
+    # Manualy define relevancies
+    
+    #relevancies = {"0":[0,1,0],
+    #               "1":[1,0,1,0,1,1,1,0,0,0,0,0,1,0,1,1,0,1,1,1,0,1,1,0,0,0,1,0,1,1,1,0,1,1,0,1,1,0,1,0,0,0,0,0,1,1,1,0,1,0,1,1,0,1,1,0,0,0,0,1,0,0,1,1,0,1,1,1,0,0,1,1,1,1,0,1,0,0,1,1,0,1,0,0,0,0],
+    #               "2":[1,1,0,1,0,0,1,0,1,0,0,0,0,1,1,0,0,0,0,1,0,1,0,0,0,0,1,0,0,0,1,0,1,0,1,0,1,1,0,0,0,0,1,1,1,0,1,1,0,0,1,0,1,0,1,0,1,1,0,1,1,0,0,1,1,0,0,1,1,1,0,0,0,0,1,1,0,1,0,0,0,0,1,0,1,1,1,0,0,0,1,1,0,0,1,1,1,0,0,1,0,0,1,1,1,1,0,0,1,0,1,0,0,0,0,1,0,1,1,0,0,1,0,0,1,0,1,0,0,1,1,1,0,1,1,1,0,0,1,0,0,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,1,1,1,0,0,0,0,1,1,0,1,0,1,0,1,0,1,1,1,1,0,1,0,0,1,0,0,0,0,0,0,0,1,0,1,1,1,0,0,0,1,1,1,1,1,1,1,0,1,0,0,1,1,1,1,1,1,1,0,1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,1,0,1,0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,1,1,0,0,1,1,1,1,0,1,0,1,0,0,1,0,0,1,1,1,0,0,0,1,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,1,0,1,1,0,0,0,0,1,0,0,0,0,1,0,1,0,1,1,0,0,0,0,1,0,0,1,0,1,1,1,1,0,0,1,1,0,1,0,0,1,0,1,1,1,0,0,0,0,1,1,0,0,1,0,1,0,1,0,1,1,0,0,1,0,0,1,0,1,0,1,0,1,1,0,1,1,1,1,0,0,0,0,1,1,0,0,0,0,1,1,1,0,1,1,1,0,1,1,0,0,1,1,1,1,1,0,0,0,0,1,0,1,1,0,0,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,0,1,0,1,0,1,1,1,0,0,0,1,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,1,0,1,0,0,1,1,0,0,0,0,1,1,1,1,0,1,0,0,1,1,0,1,1,1,1,1,0,0,0,0,0,1,1,0,1,1,1,1,0,0,1,0,1,0,1,0,0,0,0,1,0,1,1,0,0,1,0,1,0,1,1,1,1,0,1,0,0,0,1,0,1,1,1,1,0,0,1,0,0,0,1,0,1,1,1,0,1,0,1,1,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,1,1,0,1,1,1,0,1,1,0,0,0,1,1,0,0,1,0,1,0,1,1,1,0,1,0,0,0,0,1,0,1,0,1,1,1,1,1,0,1,0,0,0,1,1,1,1,1,0,0,1,0,1,0,1,1,1,0,0,0,1,1,1,0,1,0,1,0,0,1,0,1,1,0,0,0,0,0,0,1,1,1,1,0,0,0,0,1,1,0,0,0,1,0,0,0,0,1,0,1,0,1,1,1,0,0,1,0,0,0,1,0,1,1,0,0,0,1,0,1,0,0,0,1,0,1,1,1,1,0,0,1,0,1,1,0,1,1,1,0,0,1,0,0,0,1,1,0,1,0,0,1,1,0,1,0,0,0,0,0,0,0,1,0,0,1,0,1,0,1,1,1,0,1,0,0,1,1,1,1,1,1,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,1,0,0,0,1,0,1,1,1,1,1,1,1,1,0,0,1,0,0,1,0,0,1,1,0,0,1,1,0,1,1,1,0,1,0,1,1,0,0,0,1,1,0,0,1,0,0,0,1,1,0,0,0,1,1,1,0,1,0,0,0,1,1,1,0,0,1,1,0,0,0,0,0,1,0,1,0,1,1,1]
+    #                }
+
+    
+    print('Precision')
+    
+    precision = {}
+    rels = {}
+    relevant = 0
+    
+    for rank in relevancies:
+        precision[rank] = 0
+        retrieved = len(relevancies[rank])
+        j = 0
+        for rel in relevancies[rank]:
+            if rel == 1:
+                j += 1
+                relevant += 1
+        precision[rank] = j / len(relevancies[rank])
+        rels[rank] = j
         
+    print (precision)     
+
+    print ('Average Precision')
+    avp = 0
+    for rank in precision:
+        avp += precision[rank] * rels[rank]
+        
+    avp = avp / relevant
+    avps.append(avp)
+
+print (avps)
+print ('DONE')
