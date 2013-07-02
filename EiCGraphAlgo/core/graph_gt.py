42fb29057383b2df2f4d823d0de6c796d41a6ba0
Laurens De Vocht <laurensdv@gmail.com>
2013-07-02 20:38:32 +0200

    removed print statement and added EiCE GT version

---
+++ EiCGraphAlgo/core/graph_gt.py
@@ -0,0 +1,186 @@
+import numpy as np
+import graph_tool.all as gt
+import matplotlib.cm
+import sys
+import math
+import random
+from itertools import islice, chain
+import logging
+
+logger = logging.getLogger('pathFinder')
+
+
+
+def resolvePath(path,resources):
+    """Resolves a path between two resources.
+    
+    **Parameters**
+    
+    path : list of numbers corresponding with resources list indices
+    resources : list of resources containing hashes to the resources
+    
+    **Returns**
+    
+    resolvedPath : sequential list of hashes of the nodes in the path
+    
+    """
+    resolvedPath = list()
+    for step in path:
+        resolvedPath.append(resources[step])
+    return resolvedPath
+
+def batch(iterable, size):
+    sourceiter = iter(iterable)
+    while True:
+        batchiter = islice(sourceiter, size)
+        yield chain([next(batchiter)], batchiter)
+
+def rolling_window(seq, window_size):
+    it = iter(seq)
+    win = [next(it) for cnt in range(window_size)] # First window
+    yield win
+    for e in it: # Subsequent windows
+        win[:-1] = win[1:]
+        win[-1] = e
+        yield win
+        
+def resolveLinks(resolvedPath,resourcesByParent):
+    """Resolves the links in a path between two resources.
+    
+    **Parameters**
+    
+    resolvedPath : list of hash corresponding with resources in the path
+    resourcesByParent : map that contains a list of hashes with the corresponding sets of children for each parent
+    
+    **Returns**
+    
+    resolvedLinks : sequential list of predicates (URIs) connecting the resources in the path
+    
+    """
+    resolvedLinks = list()
+    for iterator in rolling_window(resolvedPath, 2):
+        steps = list(iterator)
+        #logger.debug (steps)
+        if len(steps) == 2:
+            resolvedLinks.append((resourcesByParent[steps[0]][steps[1]])['uri'][1:-1])
+    return resolvedLinks
+                                                   
+def listPath(resolvedPath,resourcesByParent):
+    """Converts a resolved path containing hashes of the resources to the actual URIs of each resource"""
+    resolvedEdges = list()
+    listedPath = list()
+    for iterator in rolling_window(resolvedPath, 2):
+        steps = list(iterator)
+        #logger.debug (steps)
+        if len(steps) == 2:
+            resolvedEdges.append(resourcesByParent[steps[0]][steps[1]])
+            
+    for node in resolvedPath:
+        step = dict()
+        step['uri'] = node.strip('<>')
+        step['type'] = 'node'
+        listedPath.append(step)
+        if len(resolvedEdges) > 0:
+            step = dict()
+            step['uri'] = resolvedEdges[0]['uri'].strip('<>')
+            step['type'] = 'link'
+            step['inverse'] = resolvedEdges[0]['inverse']
+            listedPath.append(step)
+            del resolvedEdges[0]
+    return listedPath
+
+def pathExists(M):
+    """Checks whether an adjacency matrix M contains a path or not"""
+    return gt.shortest_distance(M)
+
+def pathLength(pathFinder):
+    """Checks the length of a path if it exists in the given PathFinder class"""
+    G = pathFinder.getGraph()
+    try:
+        if pathExists(G):
+            target = G.vertex(1)
+            G, weight = buildWeightedGraph(pathFinder)
+            touch_v = G.new_vertex_property("bool")
+            touch_e = G.new_edge_property("bool")
+            dist, pred = gt.astar_search(G, G.vertex(0), weight,
+                                 VisitorExample(touch_v, touch_e, target),
+                                 heuristic=lambda v: pathFinder.jaccard(v, target))
+            return dist
+        else:
+            return -1
+    except:
+        logger.error (sys.exc_info())
+        return None
+
+def path(pathFinder):
+    """Computes the astar path if it exists in the given PathFinder class"""
+    G = pathFinder.getGraph()
+    target = G.vertex(1)
+    try:
+        if pathExists(G):
+            G, weight = buildWeightedGraph(pathFinder)
+            touch_v = G.new_vertex_property("bool")
+            touch_e = G.new_edge_property("bool")
+            dist, pred = gt.astar_search(G, G.vertex(0), weight,
+                                 VisitorExample(touch_v, touch_e, target),       
+                                 heuristic=lambda v: pathFinder.jaccard(v, target))
+            return pred
+            #return list(nx.all_simple_paths(G,0,1,cutoff=8))
+        else:
+            return None
+        
+    except:
+        logger.error (sys.exc_info())
+        return None
+    
+def buildWeightedGraph(pathFinder):
+    """Computes the weights for the links in the given PathFinder class"""
+    G = pathFinder.getGraph()
+    weight = G.new_edge_property("double")
+    for e in G.edges():
+        weight[e] = math.log(e.source().degree()+e.target().degree())
+    return G, weight
+
+class VisitorExample(gt.AStarVisitor):
+
+    def __init__(self, touched_v, touched_e, target):
+        self.touched_v = touched_v
+        self.touched_e = touched_e
+        self.target = target
+
+    def discover_vertex(self, u):
+        self.touched_v[u] = True
+
+    def examine_edge(self, e):
+        self.touched_e[e] = True
+
+    def edge_relaxed(self, e):
+        if e.target() == self.target:
+            raise gt.StopSearch()
+    
+def visualize(pathFinder, source=False, target=False):
+    g, weight = buildWeightedGraph(pathFinder)
+    ecolor = g.new_edge_property("string")
+    ewidth = g.new_edge_property("double")
+    ewidth.a = 1
+    touch_v = g.new_vertex_property("bool")
+    touch_e = g.new_edge_property("bool")
+    if not source:
+        source = g.vertex(0)
+    if not target:
+        target = g.vertex(1)
+    dist, pred = gt.astar_search(g, source, weight,
+                                 VisitorExample(touch_v, touch_e, target),
+                                 heuristic=lambda v: pathFinder.jaccard(v, target))
+    for e in g.edges():
+        ecolor[e] = "blue" if touch_e[e] else "black"
+    v = target
+    while v != g.vertex(0):
+        p = g.vertex(pred[v])
+        for e in v.out_edges():
+            if e.target() == p:
+                ecolor[e] = "#a40000"
+                ewidth[e] = 3
+        v = p
+    gt.graph_draw(g, output_size=(300, 300), vertex_fill_color=touch_v, edge_color=ecolor,
+                  edge_pen_width=ewidth, output="astar-demo.pdf")
---
+++ EiCGraphAlgo/core/pathfinder_gt.py
@@ -0,0 +1,291 @@
+import numpy as np
+import scipy
+#import networkx as nx
+import graph_tool.all as gt
+from scipy import linalg, spatial
+from core import graph_gt, resourceretriever_gt
+import time, gc, sys, logging
+from core.worker_pool import Worker
+from core.resourceretriever_gt import Resourceretriever
+
+class PathFinder:
+    """This class contains the adjacency matrix and provides interfaces to interact with it.
+    Besides the adjacency matrix it also holds the fetched resources in a hash set.
+    """
+
+    def __init__(self,s1,s2,threshold=1.1):
+        """Initialization of all required containers"""
+        self.logger = logging.getLogger('pathFinder')
+        self.query_log = logging.getLogger('query')
+        self.worker = Worker()
+        self.resources_s1 = set()
+        self.resources_s2 = set()
+        self.resources = dict()
+        self.resources_inverse_index = dict()
+        self.resources_by_parent = dict()   
+        self.storedResources = dict()  
+        self.threshold = threshold
+        self.checked_resources = 0
+        self.resourceretriever = Resourceretriever()
+        self.iteration = 0
+        self.initMatrix(s1, s2)
+
+    def initMatrix(self, source1, source2):
+        """Initialization of the adjacency matrix based on input source and destination."""
+        self.query_log.info('Path between {0} and {1}'.format(source1,source2))
+        s1 = '<%s>' % source1
+        s2 = '<%s>' % source2
+
+        self.stateGraph = gt.Graph(directed=False)
+        v1 = self.stateGraph.add_vertex()
+        v2 = self.stateGraph.add_vertex()
+        self.stateGraph.add_edge(v1, v2)
+        self.resources[self.stateGraph.vertex_index(v1)] = s1
+        self.resources_s1.add(s1)
+        self.resources[self.stateGraph.vertex_index(v2)] = s2
+        self.resources_s2.add(s2)
+        self.iteration += 1
+        return self.stateGraph
+
+    def iterateMatrix(self, blacklist=set(), additionalRes = set()):
+        """Iteration phase,
+        During this phase the children of the current bottom level nodes are fetched and added to the hashed set.
+        
+        **Parameters**
+    
+        blacklist : set, optional (default = empty)
+            set of resources predicates to exclude from the pathfinding algorithm
+        
+        additionalResources : set, optional (default = empty)
+            set of resources to include anyway in the next iteration
+    
+        **Returns**
+        
+        response : stateGraph
+            contains the updated stategraph after fetching new resources
+        """
+        self.logger.info ('--- NEW ITERATION ---')
+        self.logger.info ('Existing resources {0}'.format(str(len(self.resources))))
+        self.logger.info ('Indexed resources by parents {0}'.format(str(len(self.resources_by_parent))))
+        self.logger.info ('Grandmother: {0}'.format(self.resources[0]))
+        self.logger.info ('Grandfather: {0}'.format(self.resources[1]))
+        self.logger.info ('--- --- ---')
+        
+        start = time.clock()
+        prevResources = set()
+        additionalResources = set()
+        
+        for key in self.resources:
+            prevResources.add(self.resources[key])
+        
+        self.worker.startQueue(self.resourceretriever.fetchResource, num_of_threads=32)
+        
+        if len(additionalRes) == 0: 
+            
+            for resource in prevResources:
+                item = [resource, self.resources_by_parent, additionalResources, blacklist]
+                self.worker.queueFunction(self.resourceretriever.fetchResource, item)
+            
+            self.worker.waitforFunctionsFinish(self.resourceretriever.fetchResource)
+        
+        else:
+            self.logger.info('Special search iteration: Deep search')
+            for resource in additionalRes:
+                
+                item = [resource, self.resources_by_parent, additionalResources, blacklist]
+                self.worker.queueFunction(self.resourceretriever.fetchResource, item)
+                
+            self.worker.waitforFunctionsFinish(self.resourceretriever.fetchResource)
+            
+        
+        toAddResources = list(additionalResources - prevResources)    
+        #toAddResources = filter(resourceretriever.isResource, toAddResources)
+        
+        gc.collect()
+        
+        self.logger.info('Updated indexed resources with parents {0}'.format(str(len(self.resources_by_parent))))    
+            
+        self.logger.info ('Total resources: %s' % str(len(toAddResources)))
+
+        self.checked_resources += len(additionalResources)
+            
+        halt1 = time.clock()
+        self.logger.info ('resource gathering: %s' % str(halt1 - start))
+        #self.stateGraph = gt.Graph()
+        
+        [self.buildGraph(resource, toAddResources) for resource in toAddResources]
+        halt2 = time.clock()
+        self.logger.info ('graph construction: %s' % str(halt2 - halt1))
+        
+        #For next iteration, e.g. if no path was found
+        #Check for singular values to reduce dimensions of existing resources
+        self.storedResources.update(self.resources)
+        
+        if not graph_gt.pathExists(self.stateGraph) and self.iteration > 1:
+            try:
+                self.logger.info ('reducing matrix')
+                self.logger.debug (len(self.stateGraph))
+                k = np.int((1-np.divide(1,self.iteration))*1000)
+                h = gt.pagerank(self.stateGraph)
+                #h = (nx.pagerank_scipy(nx.Graph(self.stateGraph), max_iter=100, tol=1e-07))
+                #h = (nx.hits_scipy(nx.Graph(self.stateGraph), max_iter=100, tol=1e-07))
+                res = list(sorted(h, key=h.__getitem__, reverse=True))
+                self.logger.debug(k)
+                
+                #u, s, vt = scipy.linalg.svd(self.stateGraph.astype('float32'), full_matrices=False)
+                
+                
+                #rank = resourceretriever.rankToKeep(u, s, self.threshold)
+                #unimportant resources are unlikely to provide a path
+                #unimportant = resourceretriever.unimportantResources(u, rank, s)
+                #important = resourceretriever.importantResources(u, rank)
+
+                #print ('error ratio:')                
+                #print (np.divide(len(unimportant & important)*100,len(important)))
+                unimportant = res[k:]
+                self.resources = resourceretriever_gt.removeUnimportantResources(unimportant, self.resources)            
+                halt3 = time.clock()
+                self.logger.info ('rank reducing: %s' % str(halt3 - halt2))
+                self.logger.info('Updated resources amount: %s' % str(len(self.resources)))
+            except:
+                self.logger.error ('Graph is empty')
+                self.logger.error (sys.exc_info())
+        
+        self.logger.info ('total %s' % str(time.clock()-start))
+        self.logger.info ('=== === ===')
+        self.iteration+=1
+        return self.stateGraph
+    
+    def iterateOptimizedNetwork(self, k = 4):
+        for i in self.resources:
+            resource = self.resources[i]
+            self.resources_inverse_index[resource] = i
+            if any(e in self.resources_s1 for e in self.resources_by_parent[resource] ):
+                self.resources_s1.add(resource)
+            elif any(e in self.resources_s2 for e in self.resources_by_parent[resource] ):
+                self.resources_s2.add(resource)
+            else:
+                self.logger.warning ('resource %s does not belong to any parent' % resource)
+          
+        start = self.findBestChilds([self.resources_inverse_index[resource] for resource in self.resources_s1], k)
+        dest = self.findBestChilds([self.resources_inverse_index[resource] for resource in self.resources_s2], k)
+        
+        childs = dict() 
+        childs['start'] = [self.resources[i] for i in start]
+        childs['dest'] = [self.resources[i] for i in dest]
+        return childs
+    
+    def findBestChilds(self,nodes,k = 4):
+        node_list = dict()
+        i = 0
+        for node in nodes:
+            node_list[i] = node
+            i += 1
+            
+        self.stateGraph = gt.Graph()
+        
+        [self.buildSubGraph(node, nodes, node_list) for node in nodes]
+
+        try:
+            self.logger.debug (len(self.stateGraph))
+            h = gt.pagerank(self.stateGraph)
+            
+            res = list(sorted(h, key=h.__getitem__, reverse=True))
+
+            important = res[:k]          
+        except:
+            self.logger.error ('Graph is empty')
+            self.logger.error (sys.exc_info())
+        
+        dereffed_list = set([self.sub(i, node_list) for i in important])
+        dereffed_list.discard(0)
+        dereffed_list.discard(1)
+        return list(dereffed_list)
+    
+    def jaccard_node(self,nodeA,nodeB):
+        resA = frozenset(self.resources_by_parent[self.resources[nodeA]])
+        resB = frozenset(self.resources_by_parent[self.resources[nodeB]])
+        return 1-np.divide(len(resA & resB),len(resA | resB))  
+    
+    def jaccard_index (self,string1, string2):
+        """ Compute the Jaccard index of two strings, in an efficient way.
+        """
+        set1, set2 = set(string1), set(string2)
+        n = len(set1.intersection(set2))
+        jacc = n / float(len(set1) + len(set2) - n)
+        return jacc
+
+
+    def jaccard_distance (self,string1, string2):
+        """ Compute the Jaccard distance between two strings.
+        """
+        return 1.0 - self.jaccard_index(string1, string2)
+    
+    def jaccard(self,nodeA,nodeB):
+        """Computes the jaccard between two nodes."""
+        respbA = self.resources_by_parent[self.resources[nodeA]].values()
+        respbB = self.resources_by_parent[self.resources[nodeB]].values()
+        predA = set()
+        predB = set()
+        for link in respbA:
+            predA.add(link['uri'])
+        for link in respbB:
+            predB.add(link['uri'])
+        return scipy.spatial.distance.jaccard(np.array(predA), np.array(predB))    
+        #return 1-np.divide(len(predA & predB),len(predA | predB))       
+    
+    def buildGraph(self, resource, toAddResources):
+        vi = self.stateGraph.add_vertex()
+        """Builds a graph based on row number i and size n"""
+        [self.matchResource(vi, self.stateGraph.vertex_index(vi), resource, self.stateGraph) for resource in toAddResources]
+        
+    def buildSubGraph(self, resource, toAddResources, sub_index):
+        vi = self.stateGraph.add_vertex()
+        """Builds a graph based on row number i and size n"""
+        [self.matchResource(vi, self.stateGraph.vertex_index(vi), resource, self.stateGraph, sub_index=sub_index) for resource in toAddResources]
+        
+    def sub(self, i, sub_index = None):
+        if sub_index == None:
+            return i
+        else:
+            return sub_index[i]
+        
+    def matchResource(self, vi, i, resource, stateGraph, sub_index = None):
+        vj = stateGraph.add_vertex()
+        j = stateGraph.vertex_index(vj)
+        """Matches each resource with row and column number i and j in a row from the adjacency matrix"""
+        try:
+            if i == j:
+                stateGraph.add_edge(vi,vj)
+            elif not self.resources[self.sub(j,sub_index)] in self.resources_by_parent:
+                pass
+            elif i in self.resources:
+                if self.resources[self.sub(i,sub_index)] in self.resources_by_parent[self.resources[self.sub(j,sub_index)]]:
+                    stateGraph.add_edge(vi,vj)
+                else:
+                    pass
+            else:
+                pass
+        
+        except:
+            self.logger.error ('error %s not found in list of resources' % str(j))
+            self.logger.error (self.resources)
+            self.logger.error (sys.exc_info())
+        self.resources[j] = resource
+            
+    
+    def resourceFetcher(self):
+        q = self.worker.getQueue(self.resourceFetcher)
+        while True:
+            item = q.get()
+            self.resourceretriever.fetchResource(item[0], item[1], item[2], item[3])
+            q.task_done()
+                
+    def getResourcesByParent(self):
+        return self.resources_by_parent
+     
+    def getGraph(self):
+        return self.stateGraph    
+    
+    def getResources(self):
+        return self.storedResources
